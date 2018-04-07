2cf5593c6271f2bfdd5dd086e75a5f2ca12564ba
Johnny Zhu <zhujohnny@users.noreply.github.com>
2018-04-07 15:05:30 -0700

    Changed config vars for Heroku

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -15,7 +15,7 @@ class App extends React.Component {
 
   componentDidMount() {
     $.ajax({
-      url: 'http://localhost:1128/repos',
+      url: `${HEROKU-URL}/repos`,
       method: 'GET',
       success: data => { this.setState({repos: data}); },
       error: () => console.log('GET error!')
@@ -24,7 +24,7 @@ class App extends React.Component {
 
   search (term) {
     $.ajax({
-      url: 'http://localhost:1128/repos',
+      url: `${HEROKU-URL}/repos`,
       method: 'POST',
       contentType: 'text/plain',
       data: term,
